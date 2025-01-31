898729d2e2e832c1a3c31022df6fe827c8d089fa
vandy13 <vsharma18519@gmail.com>
2025-01-31 15:37:34 +0000

    export from folder

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // import using ESmodule
-const {multi} = require('./secondApp');
-const {sum} = require('./sum');
+const {multi, sum, sub} = require('./mod');
 
 console.log('I am first app file of this node learning project')
 
 sum(2,10);
+sub(2, 10);
 console.log(multi(2, 10));
