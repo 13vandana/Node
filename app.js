13e993d38dfee2b1405ab20cea1e6705d231fc44
vandy13 <vsharma18519@gmail.com>
2025-02-04 21:02:35 +0000

    differnt types of https call

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 // import using ESmodule
+const  https = require('https');
+const  axios = require('axios');
+const fetch = require('node-fetch');
+// const fs = require('fs');
+const crypto = require('node:crypto');
+
 const {multi, sum, sub} = require('./mod');
+
+https.get('https://jsonplaceholder.typicode.com/posts', (res) =>{
+    console.log('https')});
+axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then((res) =>{
+        console.log('axios');
+    });
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res) =>{
+        console.log('fetch');
+    });
+// const file = fs.readFileSync('./test.txt', 'utf8');
+
+// console.log('File', file);
+const key = crypto.pbkdf2Sync('munmun', 'sharma', 9000000, 64, 'sha512' );
+console.log('Key: ', key);
 
 console.log('I am first app file of this node learning project')
 
